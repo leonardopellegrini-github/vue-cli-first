957b0d59daf3770265eb2879d2a84fd0987fed36
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-05-12 16:55:47 +0200

    cambio nome file

---
+++ src/components/JumbotronHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="jumcar">
+    <h2>sono un jumbotron</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'jumbotronPrimo',
+}
+</script>
+
+<style>
+
+
+</style>
