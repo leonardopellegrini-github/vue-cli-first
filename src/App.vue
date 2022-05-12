f7cd6632cd5cac75f1ee785e229f69985355560d
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-05-12 15:45:28 +0200

    inserimento npm

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <titleFile />
+  </div>
+</template>
+
+<script>
+
+import title from '@/components/title'
+
+export default{
+  name: 'App',
+  components: {
+    titleFIle,
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/title.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Il mio Primo progetto node.js</h1>
+</template>
+
+<script>
+export default {
+  name: 'titoloFile'
+}
+</script>
+
+<style>
+
+</style>
